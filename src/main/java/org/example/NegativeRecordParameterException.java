b4eefaaaa93d70da29c7fabc26eb6cd4ea3a7bee
Milosz Kacki <s197864@student.pg.edu.pl>
2025-10-06 23:08:47 +0200

    Basic implementation of the parallelogram record

---
+++ src/main/java/org/example/NegativeRecordParameterException.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class NegativeRecordParameterException extends RuntimeException{
+    //Records for the exercise cannot have negative values
+}
---
+++ src/main/java/org/example/Record.java
@@ -0,0 +1,56 @@
+package org.example;
+
+public class Record {
+
+    //This is a record of a parallelogram
+    //          ____________________
+    //         /                   /
+    //        /                   /
+    //       /                   /
+    //      /___________________/    :3
+
+
+
+    //side lengths
+    private int a;
+    private int b;
+
+    //angle
+    private float angle;
+
+    public void Record(int a, int b, float angle){
+        setA(a);
+        setB(b);
+        setAngle(angle);
+    }
+
+    //setters
+    public void setA(int a) {
+        if (a > 0) this.a = a;
+        else
+            throw new NegativeRecordParameterException();
+    }
+
+    public void setB(int b) {
+        if (a > 0) this.b = b;
+        else
+            throw new NegativeRecordParameterException();
+    }
+
+    public void setAngle(float angle) {
+        if (a > 0) this.angle = angle;
+        else
+            throw new NegativeRecordParameterException();
+    }
+
+    //getters
+    public int getA(){
+        return this.a;
+    }
+    public int getB(){
+        return this.b;
+    }
+    public float getAngle(){
+        return this.angle;
+    }
+}
