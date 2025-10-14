3908d22b2395fc0876a05be0b1aa57301147fc2a
Milosz Kacki <s197864@student.pg.edu.pl>
2025-10-14 13:56:29 +0200

    Updated comments

--- src/main/java/org/example/Sorting.java
+++ src/main/java/org/example/Sorting.java
@@ -74,6 +74,9 @@ public class Sorting {
 
         while(IntStream.of(numbersOfRuns).sum() > 1){
             //merge tapes
+            //note that the bigger fibo tape will always be left with fibonacciable run amount
+            //TODO: what if so many runs on the mergedIinto tape get merged,
+            //that it will end up with less runs than biggerFiboTape
             mergeTapesFibo(biggerFiboTapeIndex,smallerFiboTapeIndex,emptyTapeIndex);
             //update tape designations
             int tmp = smallerFiboTapeIndex;
@@ -100,87 +103,5 @@ public class Sorting {
     private void splitIntoRunsFibo(int indexFrom, int indexToBigger, int indexToSmaller){
     }
 
-    //This shit doesnt work, need to be rewritten
-    //Method "takes" a single run from two files and merges them into one run on the destination file
-    //this method should not be called if one of the "from" tapes is empty TODO Exception?
-    //TODO runs counting
-    /*
-    private void mergeRuns(int indexFrom1, int indexFrom2, int indexTo){
-
-        Record record1,record2, record1Last, record2Last;
-
-        //used to signal that one of the runs has ended
-        //and on which tape did this occur.
-        //"-1" means no run ended yet
-        int runEnded = -1;
-
-        //if(!Tapes.get(indexFrom1).isEmpty())
-            record1 = Tapes.get(indexFrom1).popRecord();
-        //else
-            runEnded = 1;
-        //if(!Tapes.get(indexFrom2).isEmpty())
-            record2 = Tapes.get(indexFrom2).popRecord();
-        //else
-        //    if (runEnded != -1)
-        //return;
-
-        //Merging
-        while(true){
-            if(runEnded == 2 || comparator.compare(record1,record2) > 0){
-
-                Tapes.get(indexTo).saveRecord(record1);
-
-                //if this was the last record in file,
-                //get the run from the other file or end merging
-                if (Tapes.get(indexFrom1).isEmpty()){
-                    //"the run has ended and if the other one did too, stop the merge"
-                    if (runEnded == -1){
-                        runEnded = 1;
-                        continue;
-                    }
-                    break;
-                }
-
-                record1Last = record1;
-                record1 = Tapes.get(indexFrom1).popRecord();
-
-                // if record is less than the last record on tape, the run has ended on the tape
-                // put back the first record of the new run,
-                // get the run from the other file or end merging
-                if(comparator.compare(record1,record1Last) < 0){
-                    Tapes.get(indexFrom1).saveRecord(record1);
-                    if (runEnded == -1){
-                        runEnded = 1;
-                        continue;
-                    }
-                    break;
-                }
-            }
-            else{
-                Tapes.get(indexTo).saveRecord(record2);
-
-                if (Tapes.get(indexFrom2).isEmpty()){
-                    if (runEnded == -1){
-                        runEnded = 2;
-                        continue;
-                    }
-                    break;
-                }
-
-                record2Last = record2;
-                record2 = Tapes.get(indexFrom2).popRecord();
-
-                if(comparator.compare(record2,record2Last) < 0){
-                    Tapes.get(indexFrom2).saveRecord(record2);
-
-                    if (runEnded == -1){
-                        runEnded = 2;
-                        continue;
-                    }
-                    break;
-                }
-            }
-        }
-    }
-*/
+    
 }
